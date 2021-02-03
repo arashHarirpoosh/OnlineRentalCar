456dff99bbe78f6a918006f9493bf544b769a206
arashHarirpoosh <aharirpoosh@gmail.com>
2021-02-03 23:38:34 +0330

    EFStoreRepository And IStoreRepository Added

---
+++ CarRentalProject/Models/EFStoreRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarRentalProject.Models
+{
+    public class EFStoreRepository : IStoreRepository
+    {
+        private StoreDbContext context;
+
+        public EFStoreRepository(StoreDbContext ctx)
+        {
+            context = ctx;
+        }
+
+        public IQueryable<Car> Cars => context.Cars;
+
+        public void CreateProduct(Car p)
+        {
+            context.Add(p);
+            context.SaveChanges();
+        }
+
+        public void DeleteProduct(Car p)
+        {
+            context.Remove(p);
+            context.SaveChanges();
+        }
+
+        public void SaveProduct(Car p)
+        {
+            context.SaveChanges();
+        }
+    }
+}
+
---
+++ CarRentalProject/Models/IStoreRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarRentalProject.Models
+{
+    public interface IStoreRepository
+    {
+
+        IQueryable<Car> Cars { get; }
+
+        void SaveProduct(Car p);
+        void CreateProduct(Car p);
+        void DeleteProduct(Car p);
+    }
+}
