71d5cda51d533a283b99c20bce836b865dbd3ec2
arshyajabbi <arshyajabbari@gmail.com>
2021-02-04 16:56:19 +0330

    Account Controller Added

---
+++ CarRentalProject/Controllers/AccountController.cs
@@ -0,0 +1,81 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using CarRentalProject.Models.ViewModels;
+
+namespace CarRentalProject.Controllers
+{
+    public class AccountController : Controller
+    {
+        private UserManager<IdentityUser> userManager;
+        private SignInManager<IdentityUser> signInManager;
+        private RoleManager<IdentityRole> RoleManager;
+
+        public AccountController(UserManager<IdentityUser> userMgr,
+                SignInManager<IdentityUser> signInMgr, RoleManager<IdentityRole> roleManager)
+        {
+            userManager = userMgr;
+            signInManager = signInMgr;
+            RoleManager = roleManager;
+        }
+
+        public ViewResult Login(string returnUrl)
+        {
+            return View(new LoginModel
+            {
+                ReturnUrl = returnUrl
+            });
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginModel loginModel)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityUser user =
+                    await userManager.FindByNameAsync(loginModel.Name);
+                if (user != null)
+                {
+                    await signInManager.SignOutAsync();
+                    if ((await signInManager.PasswordSignInAsync(user,
+                            loginModel.Password, false, false)).Succeeded)
+                    {
+                        return Redirect(loginModel?.ReturnUrl ?? "/Admin");
+                    }
+                }
+            }
+            ModelState.AddModelError("", "Invalid name or password");
+            return View(loginModel);
+        }
+
+        public ViewResult Signup(string returnUrl)
+        {
+            return View(new SignupModel
+            {
+                ReturnUrl = returnUrl
+            });
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Signup(SignupModel signupModel)
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
+            ModelState.AddModelError("", "Invalid name or password");
+            return View(signupModel);
+        }
+
+
+        [Authorize]
+        public async Task<RedirectResult> Logout(string returnUrl = "/")
+        {
+            await signInManager.SignOutAsync();
+            return Redirect(returnUrl);
+        }
+    }
+}
---
+++ CarRentalProject/Models/ViewModels/SignupModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarRentalProject.Models.ViewModels
+{
+    public class SignupModel
+    {
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 2)]
+        [Display(Name = "Username")]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 3)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare("Password", ErrorMessage = "Confirm password doesn't match, Type again !")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        public string IdNumber { get; set; }
+
+        [Required]
+        public bool AcceptPrivacyPolicy { get; set; }
+
+        [Required]
+        [Phone]
+        [Display(Name = "Phone Number")]
+        public string PhoneNumber { get; set; }
+
+        public string ReturnUrl { get; set; } = "/";
+    }
+}
