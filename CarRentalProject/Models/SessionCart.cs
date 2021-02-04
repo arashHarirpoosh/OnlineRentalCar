c24bc93b5a538f5a4907a74e58744e39bc3b147e
arashHarirpoosh <aharirpoosh@gmail.com>
2021-02-04 17:49:27 +0330

    SessionCart And StoreDbContext Added

---
+++ CarRentalProject/Models/SessionCart.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using CarRentalProject.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace CarRentalProject.Models
+{
+    public class SessionCart : Cart
+    {
+
+        public static Cart GetCart(IServiceProvider services)
+        {
+            ISession session = services.GetRequiredService<IHttpContextAccessor>()?
+                .HttpContext.Session;
+            SessionCart cart = session?.GetJson<SessionCart>("Cart")
+                ?? new SessionCart();
+            cart.Session = session;
+            return cart;
+        }
+
+        [JsonIgnore]
+        public ISession Session { get; set; }
+
+        public override void AddItem(Car car, int quantity)
+        {
+            base.AddItem(car, quantity);
+            Session.SetJson("Cart", this);
+        }
+
+        public override void RemoveLine(Car car)
+        {
+            base.RemoveLine(car);
+            Session.SetJson("Cart", this);
+        }
+
+        public override void Clear()
+        {
+            base.Clear();
+            Session.Remove("Cart");
+        }
+    }
+}
---
+++ CarRentalProject/Models/StoreDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarRentalProject.Models
+{
+    public class StoreDbContext : DbContext
+    {
+
+        public StoreDbContext(DbContextOptions<StoreDbContext> options)
+            : base(options) { }
+
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+        internal void AttachRange(IEnumerable<Car> enumerable)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
