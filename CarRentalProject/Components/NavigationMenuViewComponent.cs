4a1ce7224ed2079519a344c90fb54db78d018d79
arshyajabbi <arshyajabbari@gmail.com>
2021-02-06 13:30:47 +0330

    Car Components Added

---
+++ CarRentalProject/Components/NavigationMenuViewComponent.cs
@@ -0,0 +1,28 @@
+﻿using CarRentalProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarRentalProject.Components
+{
+    public class NavigationMenuViewComponent : ViewComponent
+    {
+        private IStoreRepository repository;
+
+        public NavigationMenuViewComponent(IStoreRepository repo)
+        {
+            repository = repo;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.SelectedCategory = RouteData?.Values["category"];
+            return View(repository.Cars
+                .Select(x => x.Category)
+                .Distinct()
+                .OrderBy(x => x));
+        }
+    }
+}
