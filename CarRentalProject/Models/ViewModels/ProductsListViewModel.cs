19b8071c9f45c7d00c76526f323bde3a1f1c8548
arashHarirpoosh <aharirpoosh@gmail.com>
2021-02-06 14:03:40 +0330

    PagingInfo And ProductsListViewModel Added

---
+++ CarRentalProject/Models/ViewModels/ProductsListViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarRentalProject.Models.ViewModels
+{
+    public class ProductsListViewModel
+    {
+        public IEnumerable<Car> Cars { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+        public string CurrentCategory { get; set; }
+    }
+}
